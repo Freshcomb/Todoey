95a23e993946c18f29668818a60ec5621a20077d
Macbook Air <macbookair@macbooks-macbook-air.local>
2018-09-30 16:14:11 -0700

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Macbook Air on 2018-09-30.
+//  Copyright © 2018 Buh Inc. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done: Bool = false
+    
+    
+    
+}
